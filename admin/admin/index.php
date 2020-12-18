eec127c3f84583fb4e48113ff191e45af583ac96
Umut Can Arda <umutkonurinso@gmail.com>
2020-12-18 19:36:39 +0300

    update2

--- admin/admin/index.php
+++ admin/admin/index.php
@@ -32,16 +32,14 @@ include 'komutlar.php';
 
         <div class="container-fluid" id="container-wrapper">
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Lol</h1>
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="./">Anasayfa</a></li>
-            </ol>
+            <h1 class="h3 mb-0 text-gray-800">League of Legends</h1>
+
           </div>
 
           <div class="row mb-3">
             <!-- Earnings (Monthly) Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card h-100">
+          <a href="eloboostsiparis">    <div class="card h-100">
                 <div class="card-body">
                   <div class="row align-items-center">
                     <div class="col mr-2">
@@ -53,18 +51,19 @@ include 'komutlar.php';
                       </div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-calendar fa-2x text-primary"></i>
+                      <i class="fas fa-money-bill-alt fa-2x text-primary"></i>
                     </div>
                   </div>
                 </div>
-              </div>
+              </div></a>
             </div>
             <div class="col-xl-3 col-md-6 mb-4">
+            <a href="duoboostsiparis">
               <div class="card h-100">
                 <div class="card-body">
                   <div class="row align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-uppercase mb-1">Elo Boost Sipariş</div>
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Duo Boost Sipariş</div>
                       <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $urunsor3yaz['sayi2']." TL"; ?></div>
                       <div class="mt-2 mb-0 text-muted text-xs">
                         <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> <?php echo $urunsaydu; ?></span>
@@ -72,68 +71,69 @@ include 'komutlar.php';
                       </div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-calendar fa-2x text-primary"></i>
+                      <i style="color:#3abaf4!important" class="fas fa-money-bill-alt fa-2x text-primary"></i>
                     </div>
                   </div>
                 </div>
               </div>
+            </a>
             </div>
             <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card h-100">
+            <a href="ilk10macsiparis">  <div class="card h-100">
                 <div class="card-body">
                   <div class="row align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-uppercase mb-1">Elo Boost Sipariş</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">İlk 10 Maç Sipariş</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $urunsor4yaz['sayi']." TL"; ?></div>
                       <div class="mt-2 mb-0 text-muted text-xs">
-                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 2</span>
+                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> <?php echo $urunsay10; ?></span>
                         <span>Toplam Sipariş Sayısı</span>
                       </div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-calendar fa-2x text-primary"></i>
+                      <i style="color:#66bb6a!important" class="fas fa-money-bill-alt fa-2x text-primary"></i>
                     </div>
                   </div>
                 </div>
-              </div>
+              </div></a>
             </div>
             <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card h-100">
+          <a href="winboostsiparis">    <div class="card h-100">
                 <div class="card-body">
                   <div class="row align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-uppercase mb-1">Elo Boost Sipariş</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Win Boost Sipariş</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $urunsor5yaz['sayi']." TL"; ?></div>
                       <div class="mt-2 mb-0 text-muted text-xs">
-                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 2</span>
+                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> <?php echo $urunsaywin; ?></span>
                         <span>Toplam Sipariş Sayısı</span>
                       </div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-calendar fa-2x text-primary"></i>
+                      <i style="color:#ffa426!important" class="fas fa-money-bill-alt fa-2x text-primary"></i>
                     </div>
                   </div>
                 </div>
-              </div>
+              </div></a>
             </div>
             <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card h-100">
+              <a href="challengersiparis"><div class="card h-100">
                 <div class="card-body">
                   <div class="row align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-uppercase mb-1">Elo Boost Sipariş</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Challenger Boost Sipariş</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $urunsor6yaz['sayi']." TL"; ?></div>
                       <div class="mt-2 mb-0 text-muted text-xs">
-                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 2</span>
+                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> <?php echo $urunsaychl; ?></span>
                         <span>Toplam Sipariş Sayısı</span>
                       </div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-calendar fa-2x text-primary"></i>
+                      <i style="color:#b726ff!important" class="fas fa-money-bill-alt fa-2x text-primary"></i>
                     </div>
                   </div>
                 </div>
-              </div>
+              </div></a>
             </div>
 
             <!-- Area Chart -->
@@ -156,92 +156,88 @@ include 'komutlar.php';
         <div class="container-fluid" id="container-wrapper">
           <div class="d-sm-flex align-items-center justify-content-between mb-4">
             <h1 class="h3 mb-0 text-gray-800">Valorant</h1>
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item"><a href="./">Anasayfa</a></li>
-            </ol>
+
           </div>
 
           <div class="row mb-3">
             <!-- Earnings (Monthly) Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card h-100">
+          <a href="veloboostsiparis">    <div class="card h-100">
                 <div class="card-body">
                   <div class="row align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-uppercase mb-1">Earnings (Monthly)</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">$40,000</div>
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Elo Boost Sipariş</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $urunsor22yaz['sayi']." TL"; ?></div>
                       <div class="mt-2 mb-0 text-muted text-xs">
-                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
-                        <span>Since last month</span>
+                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> <?php echo $urunsayv; ?></span>
+                        <span>Toplam Sipariş Sayısı</span>
                       </div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-calendar fa-2x text-primary"></i>
+                      <i class="fas fa-money-bill-alt fa-2x text-primary"></i>
                     </div>
                   </div>
                 </div>
-              </div>
+              </div></a>
             </div>
-            <!-- Earnings (Annual) Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card h-100">
+            <a href="vduoboostsiparis">  <div class="card h-100">
                 <div class="card-body">
-                  <div class="row no-gutters align-items-center">
+                  <div class="row align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-uppercase mb-1">Sales</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">650</div>
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Duo Boost Sipariş</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $urunsor33yaz['sayi']." TL"; ?></div>
                       <div class="mt-2 mb-0 text-muted text-xs">
-                        <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 12%</span>
-                        <span>Since last years</span>
+                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> <?php echo $urunsayduv; ?></span>
+                        <span>Toplam Sipariş Sayısı</span>
                       </div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-shopping-cart fa-2x text-success"></i>
+                      <i style="color:#3abaf4!important" class="fas fa-money-bill-alt fa-2x text-primary"></i>
                     </div>
                   </div>
                 </div>
-              </div>
+              </div></a>
             </div>
-            <!-- New User Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card h-100">
+            <a href="vilk5macsiparis">  <div class="card h-100">
                 <div class="card-body">
-                  <div class="row no-gutters align-items-center">
+                  <div class="row align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-uppercase mb-1">New User</div>
-                      <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">366</div>
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">İlk 5 Maç Sipariş</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $urunsor44yaz['sayi']." TL"; ?></div>
                       <div class="mt-2 mb-0 text-muted text-xs">
-                        <span class="text-success mr-2"><i class="fas fa-arrow-up"></i> 20.4%</span>
-                        <span>Since last month</span>
+                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> <?php echo $urunsay5v; ?></span>
+                        <span>Toplam Sipariş Sayısı</span>
                       </div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-users fa-2x text-info"></i>
+                      <i style="color:#66bb6a!important" class="fas fa-money-bill-alt fa-2x text-primary"></i>
                     </div>
                   </div>
                 </div>
-              </div>
+              </div></a>
             </div>
-            <!-- Pending Requests Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card h-100">
+          <a href="vnormalmacsiparis">    <div class="card h-100">
                 <div class="card-body">
-                  <div class="row no-gutters align-items-center">
+                  <div class="row align-items-center">
                     <div class="col mr-2">
-                      <div class="text-xs font-weight-bold text-uppercase mb-1">Pending Requests</div>
-                      <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                      <div class="text-xs font-weight-bold text-uppercase mb-1">Normal Sipariş</div>
+                      <div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $urunsor55yaz['sayi']." TL"; ?></div>
                       <div class="mt-2 mb-0 text-muted text-xs">
-                        <span class="text-danger mr-2"><i class="fas fa-arrow-down"></i> 1.10%</span>
-                        <span>Since yesterday</span>
+                        <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> <?php echo $urunsaynormv; ?></span>
+                        <span>Toplam Sipariş Sayısı</span>
                       </div>
                     </div>
                     <div class="col-auto">
-                      <i class="fas fa-comments fa-2x text-warning"></i>
+                      <i style="color:#ffa426!important" class="fas fa-money-bill-alt fa-2x text-primary"></i>
                     </div>
                   </div>
                 </div>
-              </div>
+              </div></a>
             </div>
+
 
             <!-- Area Chart -->
 
--- admin/admin/komutlar.php
+++ admin/admin/komutlar.php
@@ -45,12 +45,80 @@ $urunsor2=$db->prepare("SELECT SUM(eloboostsiparis_fiyat) AS sayi FROM bolfps_el
 $urunsor2->execute();
 $urunsor2yaz= $urunsor2->fetch(PDO::FETCH_ASSOC);
 
+$urunsor4=$db->prepare("SELECT SUM(ilk10macsiparis_fiyat) AS sayi FROM bolfps_ilk10macsiparis");
+$urunsor4->execute();
+$urunsor4yaz= $urunsor4->fetch(PDO::FETCH_ASSOC);
+
+$urunsor5=$db->prepare("SELECT SUM(winboostsiparis_fiyat) AS sayi FROM bolfps_winboostsiparis");
+$urunsor5->execute();
+$urunsor5yaz= $urunsor5->fetch(PDO::FETCH_ASSOC);
+
+$urunsor6=$db->prepare("SELECT SUM(challengerboostsiparis_fiyat) AS sayi FROM bolfps_challengerboostsiparis");
+$urunsor6->execute();
+$urunsor6yaz= $urunsor6->fetch(PDO::FETCH_ASSOC);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$urunsor22=$db->prepare("SELECT SUM(valorantelosiparis_fiyat) AS sayi FROM bolfps_valorantelosiparis");
+$urunsor22->execute();
+$urunsor22yaz= $urunsor22->fetch(PDO::FETCH_ASSOC);
+
+$urunsor33=$db->prepare("SELECT SUM(valorantduoboostsiparis_fiyat) AS sayi FROM bolfps_valorantduoboostsiparis");
+$urunsor33->execute();
+$urunsor33yaz= $urunsor33->fetch(PDO::FETCH_ASSOC);
+
+$urunsor44=$db->prepare("SELECT SUM(ilk5macsiparis_fiyat) AS sayi FROM bolfps_ilk5macsiparis");
+$urunsor44->execute();
+$urunsor44yaz= $urunsor44->fetch(PDO::FETCH_ASSOC);
+
+$urunsor55=$db->prepare("SELECT SUM(normalmacsiparis_fiyat) AS sayi FROM bolfps_normalmacsiparis");
+$urunsor55->execute();
+$urunsor55yaz= $urunsor55->fetch(PDO::FETCH_ASSOC);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 $urunsor3=$db->prepare("SELECT SUM(duoboostsiparis_fiyat) AS sayi2 FROM bolfps_duoboostsiparis");
 $urunsor3->execute();
 $urunsor3yaz= $urunsor3->fetch(PDO::FETCH_ASSOC);
 
 $urunsorv=$db->prepare("SELECT * FROM bolfps_valorantelosiparis ORDER BY valorantelosiparis_id DESC");
 $urunsorv->execute();
+$urunsayv=$urunsorv->rowCount();
 
 $urunsorduo=$db->prepare("SELECT * FROM bolfps_duoboostsiparis ORDER BY duoboostsiparis_id DESC");
 $urunsorduo->execute();
@@ -58,24 +126,33 @@ $urunsaydu=$urunsorduo->rowCount();
 
 $urunsorduov=$db->prepare("SELECT * FROM bolfps_valorantduoboostsiparis ORDER BY valorantduoboostsiparis_id DESC");
 $urunsorduov->execute();
+$urunsayduv=$urunsorduov->rowCount();
 
 $urunsor10=$db->prepare("SELECT * FROM bolfps_ilk10macsiparis ORDER BY ilk10macsiparis_id DESC");
 $urunsor10->execute();
+$urunsay10=$urunsor10->rowCount();
+
 
 $urunsor5=$db->prepare("SELECT * FROM bolfps_ilk5macsiparis ORDER BY ilk5macsiparis_id DESC");
 $urunsor5->execute();
+$urunsay5v=$urunsor5->rowCount();
 
 $urunsorn=$db->prepare("SELECT * FROM bolfps_normalmacsiparis ORDER BY normalmacsiparis_id DESC");
 $urunsorn->execute();
+$urunsaynormv=$urunsorn->rowCount();
+
 
 $usersor=$db->prepare("SELECT * FROM bolfps_user ORDER BY user_id DESC");
 $usersor->execute();
 
 $urunsorwin=$db->prepare("SELECT * FROM bolfps_winboostsiparis ORDER BY winboostsiparis_id DESC");
 $urunsorwin->execute();
+$urunsaywin=$urunsorwin->rowCount();
 
 $urunsorchallenger=$db->prepare("SELECT * FROM bolfps_challengerboostsiparis ORDER BY challengerboostsiparis_id DESC");
 $urunsorchallenger->execute();
+$urunsaychl=$urunsorchallenger->rowCount();
+
 
 
 $sunucusor=$db->prepare("SELECT * FROM bolfps_sunucu ORDER BY sunucu_id DESC");
